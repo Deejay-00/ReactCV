04ad6e3a8a8826c690aaab0637ba1c381e00b341
Jamiu <jammy190@gmail.com>
2022-05-16 16:56:49 +0100

    i added the interest icon updated work experience and divide cv into sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
 import Darecv from './component/MyCV/Darecv';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHouse, faEnvelope,faPhone,faCode, faBook,faCarAlt, faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHouse,faEnvelope,faPhone,faCode, faBook,faCarAlt, faPlaneDeparture);
+
 
 function App() {
   return (
     <Darecv />
-
+    
   );
 }
 
